13ecb4a6745f7c8a5673a6ffbdcda298cc36b5cf
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-06-09 20:16:38 -0500

    Re-enabled withAuth constant

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { userType, User, Painting, PaintingProc, Category, PaintingCat, Tag, PaintingTag } = require("../models");
-// const withAuth = require("../utils/auth");
+const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
