a714c0a772a0456d0bfa56b8a129bed5f0e625ec
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-06-09 20:42:24 -0500

    Implement GET all users method

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
-const gallaryRoutes = require("./gallaryRoutes");
-const paintingRoutes = require("./paintingsRoutes");
+// const gallaryRoutes = require("./gallaryRoutes");
+// const paintingRoutes = require("./paintingsRoutes");
 
 
 router.use("/users", userRoutes);
-router.use("/paintings", paintingsRoutes);
-router.use("/gallary", gallaryRoutes);
+// router.use("/paintings", paintingsRoutes);
+// router.use("/gallary", gallaryRoutes);
 
 module.exports = router;
