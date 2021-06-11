71cbd5a0ef04c9bb0ad334be2f88df74976ff220
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-06-11 15:00:16 -0500

    Initial PaintingProc commit

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const paintingRoutes = require("./paintingRoutes");
+const paintingProcRoutes = require("./paintingProcRoutes");
 
 // Creating routes to individual JS files
 router.use("/users", userRoutes);
-router.use("/paintings", paintingRoutes)
+router.use("/paintings", paintingRoutes);
+router.use("/paintingprocs", paintingProcRoutes);
 
 module.exports = router;
