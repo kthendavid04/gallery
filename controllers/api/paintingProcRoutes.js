84de4f6a91bc69c9f468a0261baf774613fb07bd
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-06-11 15:09:37 -0500

    Added GET route for all painting procurements

--- controllers/api/paintingProcRoutes.js
+++ controllers/api/paintingProcRoutes.js
@@ -1,2 +1,24 @@
 const router = require("express").Router();
+const { PaintingProc } = require("../../models");
+
+// GET route for all procurements table
+router.get("/", async (req, res) => {
+
+    try {
+        
+        // Query for all paintings procurements and save results to variable
+        const procurements = await PaintingProc.findAll({ raw: true});
+
+        // Returns with status code 200
+        // and displays all paintings list
+        res.status(200).json(procurements);
+        
+    } catch (error) {
+        
+        // Returns with status code 500
+        // and displays error
+        res.status(500).json({ result: "Unable to get all paintings procurements"});
+    }
+});
+
 module.exports = router;
