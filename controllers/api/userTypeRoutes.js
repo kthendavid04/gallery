415db5a7b03afe497ada7b73b5afacd029c5bde6
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-06-10 20:10:15 -0500

    Updated user types model and route (POST, UPDATE)

--- controllers/api/userTypeRoutes.js
+++ controllers/api/userTypeRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { UserType } = require("../../models");
 
+// GET route for all user types
 router.get("/", async (req, res) => {
     
     try {
@@ -17,6 +18,54 @@ router.get("/", async (req, res) => {
         // Returns with status code 500
         // and displays error
         res.status(500).json("Unable to get all user types");
+    }
+});
+
+// POST create single user type
+router.post("/", async (req, res) => {
+
+    try {
+
+        // Runs a insert query and saves to variable
+        const newTypeData = await UserType.create(req.body);
+
+        // Returns code to 200 and displays new user type object
+        res.status(200).json(newTypeData);
+        
+    } catch (error) {
+        
+        // Returns with status code 500
+        // and displays error
+        res.status(500).json("Unable to create user type");
+    }
+});
+
+router.put("/:id", async (req, res) => {
+
+    try {
+
+        // Runs a update query and saves to variable with a where clause
+        const userTypeData = await UserType.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+        
+        // Checks if the userTypeData data returned successfully
+        if (!userTypeData[0]) {
+            res.status(404).json({ message: 'No user type with this id!' });
+            return;
+        }
+
+        // Returns with status code 200
+        // and displays successfully updated
+        res.status(200).json("Successfully updated");
+        
+    } catch (error) {
+
+        // Returns with status code 500
+        // and displays error
+        res.status(500).json("Unable to update user type");
         
     }
 });
