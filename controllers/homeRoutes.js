29217a9d0e628451709680d23e0413193d6e1ef6
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-06-14 18:37:26 -0500

    Added-Removed comments

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,14 +5,23 @@ const { User, Painting, PaintingProc, Category, PaintingCat, Tag, PaintingTag } 
 const withAuth = require("../utils/auth");
 const redirect = require("../utils/redirect");
 
+// GET route for the homepage
 router.get("/", async (req, res) => {
+  
   try {
+
+    // Renders the homepage handlebar
     res.render("homepage", { loggedIn: req.session.loggedIn, homepageAct: true, galleryAct: false, teamAct: false });
-  } catch (err) {
-    res.status(500).json(err);
+
+  } catch (error) {
+
+    // Returns with status code 500
+    // and displays error
+    res.status(500).json(error);
   }
 });
 
+// GET route for the gallery page, default displays newest to oldest
 router.get("/gallery", async (req, res) => {
   
   try {
@@ -66,6 +75,8 @@ router.get("/gallery", async (req, res) => {
     });
     
     // Downloads data from MySQL painting.image_data and creates file into the uploads folder
+    // Updates the original_painter property rather than ID, to first and last name
+    // Updates the image_data from BLOB binary to an actual static path
     for (const painting of paintings) {
       fs.writeFileSync(__basedir + "/uploads/" + painting.image_name, painting.image_data);
       let painterName = await User.findByPk(painting.original_painter, { attributes: ["first_name", "last_name"] });
@@ -79,11 +90,15 @@ router.get("/gallery", async (req, res) => {
     // Goes to Gallery handlebar, and pass paintings
     res.render("gallery", { paintings: allPaintings, loggedIn: req.session.loggedIn, isSelling: true, homepageAct: false, galleryAct: true, teamAct: false });
 
-  } catch (err) {
-    res.status(500).json(err);
+  } catch (error) {
+
+    // Returns with status code 500
+    // and displays error
+    res.status(500).json(error);
   }
 });
-//sorting gallery postings from oldest to newest
+
+// GET route for gallery oldest, displays arts from old -> newest
 router.get("/gallery/oldest", async (req, res) => {
   
   try {
@@ -137,6 +152,8 @@ router.get("/gallery/oldest", async (req, res) => {
     });
 
     // Downloads data from MySQL painting.image_data and creates file into the uploads folder
+    // Updates the original_painter property rather than ID, to first and last name
+    // Updates the image_data from BLOB binary to an actual static path
     for (const painting of paintings) {
       fs.writeFileSync(__basedir + "/uploads/" + painting.image_name, painting.image_data);
       let painterName = await User.findByPk(painting.original_painter, { attributes: ["first_name", "last_name"] });
@@ -150,32 +167,40 @@ router.get("/gallery/oldest", async (req, res) => {
     // Goes to Gallery handlebar, and pass paintings
     res.render("galleryOldest", { paintings: allPaintings, loggedIn: req.session.loggedIn, isSelling: true, homepageAct: false, galleryAct: true, teamAct: false });
 
-  } catch (err) {
-    res.status(500).json(err);
+  } catch (error) {
+
+    // Returns with status code 500
+    // and displays error
+    res.status(500).json(error);
   }
 });
 
-//sorting gallery postings by category
-router.get("/gallery/category", async (req, res) => {
+// GET route for gallery category, using middleware [redirect]
+// in case users try to access the site, future implementation
+router.get("/gallery/category", redirect, async (req, res) => {
   try {
     res.render("galleryCategory", { loggedIn: req.session.loggedIn });
-  } catch (err) {
-    res.status(500).json(err);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
-//sorting callery postings by tag
-router.get("/gallery/tag", async (req, res) => {
+
+// GET route for gallery tags, uusing middleware [redirect]
+// in case users try to access the site, future implementation
+router.get("/gallery/tag", redirect, async (req, res) => {
   try {
     res.render("galleryTag", { loggedIn: req.session.loggedIn });
-  } catch (err) {
-    res.status(500).json(err);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
-//sorting gallery postings on price lowest to highest
+
+// GET route for gallery pricelowtohigh, displays arts from low -> high
 router.get("/gallery/pricelowtohigh", async (req, res) => {
   
   try {
     
+    // Local scope variables
     const currentUser = req.session.hasOwnProperty("userId") ? req.session.userId : 0;
     const paintings = await Painting.findAll({
       include: [
@@ -224,6 +249,8 @@ router.get("/gallery/pricelowtohigh", async (req, res) => {
     });
 
     // Downloads data from MySQL painting.image_data and creates file into the uploads folder
+    // Updates the original_painter property rather than ID, to first and last name
+    // Updates the image_data from BLOB binary to an actual static path
     for (const painting of paintings) {
       fs.writeFileSync(__basedir + "/uploads/" + painting.image_name, painting.image_data);
       let painterName = await User.findByPk(painting.original_painter, { attributes: ["first_name", "last_name"] });
@@ -237,15 +264,20 @@ router.get("/gallery/pricelowtohigh", async (req, res) => {
       // Goes to Gallery handlebar, and pass paintings
       res.render("galleryLow", { paintings: allPaintings, loggedIn: req.session.loggedIn, isSelling: true, homepageAct: false, galleryAct: true, teamAct: false });
     
-  } catch (err) {
-    res.status(500).json(err);
+  } catch (error) {
+
+    // Returns with status code 500
+    // and displays error
+    res.status(500).json(error);
   }
 });
-//sorting gallery postings on price highest to lowest
+
+// GET route for gallery pricehightolow, displays arts from high -> low
 router.get("/gallery/pricehightolow", async (req, res) => {
   
   try {
 
+    // Local scope variables
     const currentUser = req.session.hasOwnProperty("userId") ? req.session.userId : 0;
     const paintings = await Painting.findAll({
       include: [
@@ -294,6 +326,8 @@ router.get("/gallery/pricehightolow", async (req, res) => {
     });
 
     // Downloads data from MySQL painting.image_data and creates file into the uploads folder
+    // Updates the original_painter property rather than ID, to first and last name
+    // Updates the image_data from BLOB binary to an actual static path
     for (const painting of paintings) {
       fs.writeFileSync(__basedir + "/uploads/" + painting.image_name, painting.image_data);
       let painterName = await User.findByPk(painting.original_painter, { attributes: ["first_name", "last_name"] });
@@ -304,57 +338,94 @@ router.get("/gallery/pricehightolow", async (req, res) => {
     // Copies paintings into allPaintings with serialize data
     const allPaintings = paintings.map((painting) => painting.get({plain: true}));
 
+    // Renders the handlebar
     res.render("galleryHigh", { paintings: allPaintings, loggedIn: req.session.loggedIn, isSelling: true, homepageAct: false, galleryAct: true, teamAct: false });
-  } catch (err) {
-    res.status(500).json(err);
+
+  } catch (error) {
+
+    // Returns with status code 500
+    // and displays error
+    res.status(500).json(error);
   }
 });
-//sorting gallery postings by artist name
+
+// GET route for gallery artist, using middleware [redirect]
+// in case users try to access the site, future implementation
 router.get("/gallery/artistname", redirect, async (req, res) => {
   try {
     res.render("galleryArtist", { paintings: allPaintings, loggedIn: req.session.loggedIn });
-  } catch (err) {
-    res.status(500).json(err);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
-//sorting gallery postings by artwok name
+
+// GET route for gallery artwork name, using middleware [redirect]
+// in case users try to access the site, future implementation
 router.get("/gallery/artworkname", redirect, async (req, res) => {
   try {
     res.render("galleryArtwork", { loggedIn: req.session.loggedIn });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-//directing user to the signup page if not logged in
-router.get("/signup", async (req, res) => {
-  try {
-    res.render("signup", { loggedIn: req.session.loggedIn });
-  } catch (err) {
-    res.status(500).json(err);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
-router.get("/profile", withAuth, async (req, res) => {
+// GET route to have users signup for an account
+router.get("/signup", async (req, res) => {
   try {
+
+    // Renders handlebar
+    res.render("signup", { loggedIn: req.session.loggedIn });
+
+  } catch (error) {
+
+    // Returns with status code 500
+    // and displays error
+    res.status(500).json(error);
+  }
+});
+
+// GET route to take logged in user to their personal profile
+router.get("/profile", withAuth, async (req, res) => {
+  
+  try {
+
+    // Local scope variables
     const dbUserData = await User.findOne({ where: {id: req.session.userId}})
     const user = dbUserData.get({ plain: true });
 
+    // Renders handlebar
     res.render("profile", { user, loggedIn: req.session.loggedIn });
-  } catch (err) {
-    res.status(500).json(err);
+
+  } catch (error) {
+
+    // Returns with status code 500
+    // and displays error
+    res.status(500).json(error);
   }
 });
 
+// GET route to take logged in users to post a new art piece
 router.get("/profile/newart", withAuth, async (req, res) => {
+  
   try {
+
+    // Renders handlebar
     res.render("profileNewArt", { loggedIn: req.session.loggedIn });
-  } catch (err) {
-    res.status(500).json(err);
+
+  } catch (error) {
+
+    // Returns with status code 500
+    // and displays error
+    res.status(500).json(error);
   }
 });
 
+// GET route to take logged in users to view their current arts for sale
 router.get("/profile/listed", withAuth, async (req, res) => {
+  
   try {
+
+    // Local scope variables
     const listedPaintings = await Painting.findAll({
       include: [
         {
@@ -371,24 +442,36 @@ router.get("/profile/listed", withAuth, async (req, res) => {
       }
     });
 
+    // Downloads data from MySQL painting.image_data and creates file into the uploads folder
+    // Updates the original_painter property rather than ID, to first and last name
+    // Updates the image_data from BLOB binary to an actual static path
     for (const painting of listedPaintings) {
       fs.writeFileSync(__basedir + "/uploads/" + painting.image_name, painting.image_data);
-      painting.image_data = "/uploads/" + painting.image_name;
       let painterName = await User.findByPk(painting.original_painter, { attributes: ["first_name", "last_name"] });
       painting.original_painter = painterName.first_name + " " + painterName.last_name;
+      painting.image_data = "/uploads/" + painting.image_name;
     }
 
+    // Copies listedPaintings into paintings with serialize data
     const paintings = listedPaintings.map((painting) => painting.get({plain: true}));
 
+    // Renders handlebar
     res.render("profileListed", { paintings: paintings, loggedIn: req.session.loggedIn });
-  } catch (err) {
-    res.status(500).json(err);
+
+  } catch (error) {
+
+    // Returns with status code 500
+    // and displays error
+    res.status(500).json(error);
   }
 });
 
+// GET route to take logged in users to view all their purchases
 router.get("/profile/purchased", withAuth, async (req, res) => {
+  
   try {
 
+    // Local scope variables
     const purchased = await Painting.findAll({
       include: [
         {
@@ -403,27 +486,36 @@ router.get("/profile/purchased", withAuth, async (req, res) => {
       }
     });
 
+    // Downloads data from MySQL painting.image_data and creates file into the uploads folder
+    // Updates the original_painter property rather than ID, to first and last name
+    // Updates the image_data from BLOB binary to an actual static path
     for (const painting of purchased) {
       fs.writeFileSync(__basedir + "/uploads/" + painting.image_name, painting.image_data);
-      painting.image_data = "/uploads/" + painting.image_name;
       let painterName = await User.findByPk(painting.original_painter, { attributes: ["first_name", "last_name"] });
       painting.original_painter = painterName.first_name + " " + painterName.last_name;
+      painting.image_data = "/uploads/" + painting.image_name;
     }
 
+    // Copies purchased into paintings with serialize data
     const paintings = purchased.map((painting) => painting.get({plain: true}));
 
-    console.log(paintings);
-
+    // Renders handlebars
     res.render("profilePurchased", { paintings, loggedIn: req.session.loggedIn });
-  } catch (err) {
-    res.status(500).json(err);
+
+  } catch (error) {
+
+    // Returns with status code 500
+    // and displays error
+    res.status(500).json(error);
   }
 });
 
+// GET route to take logged in users to view all their sold art
 router.get("/profile/sold", withAuth, async (req, res) => {
 
   try {
     
+    // Local scope variables
     const paintingsSold = await Painting.findAll({
       include: [
         {
@@ -435,9 +527,10 @@ router.get("/profile/sold", withAuth, async (req, res) => {
         }
       ]
     });
-    
 
     // Downloads data from MySQL painting.image_data and creates file into the uploads folder
+    // Updates the original_painter property rather than ID, to first and last name
+    // Updates the image_data from BLOB binary to an actual static path
     for (const painting of paintingsSold) {
       fs.writeFileSync(__basedir + "/uploads/" + painting.image_name, painting.image_data);
       let painterName = await User.findByPk(painting.original_painter, { attributes: ["first_name", "last_name"] });
@@ -445,18 +538,26 @@ router.get("/profile/sold", withAuth, async (req, res) => {
       painting.image_data = "/uploads/" + painting.image_name;
     }
 
+    // Copies paintingsSold into paintings with serialize data
     const paintings = paintingsSold.map((painting) => painting.get({plain: true}));
 
+    // Renders handlebar
     res.render("profileSold", {paintings, isSelling: false, loggedIn: req.session.loggedIn });
-  } catch (err) {
-    res.status(500).json(err);
+
+  } catch (error) {
+
+    // Returns with status code 500
+    // and displays error
+    res.status(500).json(error);
   }
 });
 
-router.get("/sale/:id", async (req, res) => {
+// GET route to take logged in users to view the individual art for sale
+router.get("/sale/:id", withAuth, async (req, res) => {
   
   try {
 
+    // Local scope variables
     const userId = req.session.userId;
     const paintingId = req.params.id;
     const buyerInfo = await User.findByPk(userId, { attributes: ["id", "first_name", "last_name", "address"] });
@@ -479,153 +580,58 @@ router.get("/sale/:id", async (req, res) => {
     });
     const artistData = await User.findByPk(paintingData.original_painter, {attributes: ["first_name", "last_name"]});
 
+    // Downloads data from MySQL painting.image_data and creates file into the uploads folder
     fs.writeFileSync(__basedir + "/uploads/" + paintingData.image_name, paintingData.image_data);
+
+    // Updates the image_data from BLOB binary to an actual static path
     paintingData.image_data = "/uploads/" + paintingData.image_name;
 
+    // Copies the sql query results plain
     const buyer = buyerInfo.get({plain: true})
     const painting = paintingData.get({plain: true});
     const artist = artistData.get({plain: true});
 
+    // Renders handlebar
     res.render('sale', { buyer, painting, artist, loggedIn: req.session.loggedIn });
     
-  } catch (err) {
-    res.status(500).json(err);
+  } catch (error) {
+
+    // Returns with status code 500
+    // and displays error
+    res.status(500).json(error);
   }
 });
 
+// GET route to show the team members of the project
 router.get("/meet", async (req, res) => {
+  
   try {
+
+    // Renders handlebar
     res.render('meet', { loggedIn: req.session.loggedIn, homepageAct: false, galleryAct: false, teamAct: true });
-  } catch (err) {
-    res.status(500).json(err);
+
+  } catch (error) {
+
+    // Returns with status code 500
+    // and displays error
+    res.status(500).json(error);
   }
 });
 
-// router.get("/post/:id", async (req, res) => {
-//   try {
-//     const postData = await Post.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: User,
-//           attributes: ["name"],
-//         },
-//       ],
-//     });
-
-//     const post = postData.get({ plain: true });
-
-//     res.render("post", {
-//       ...post,
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-// comments
-
-// router.get("/", async (req, res) => {
-//   try {
-//     // Get all projects and JOIN with user data
-//     const commentsData = await Comments.findAll({
-//       include: [
-//         {
-//           model: User,
-//           attributes: ["name"],
-//         },
-//       ],
-//     });
-
-//     // Serialize data so the template can read it
-//     const comments = commentsData.map((post) => comments.get({ plain: true }));
-
-//     // Pass serialized data and session flag into template
-//     res.render("homepage", { 
-//       comments, 
-//       logged_in: req.session.logged_in 
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-// router.get("/comments/:id", async (req, res) => {
-//   try {
-//     const commentsData = await Comments.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: User,
-//           attributes: ["name"],
-//         },
-//       ],
-//     });
-
-//     const comments = commentsData.get({ plain: true });
-
-//     res.render("comments", {
-//       ...comments,
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-
-
-// Use withAuth middleware to prevent access to route
-// router.get("/profile", withAuth, async (req, res) => {
-//   try {
-//     // Find the logged in user based on the session ID
-//     const userData = await User.findByPk(req.session.user_id, {
-//       attributes: { exclude: ["password"] },
-//       include: [{ model: Post }],
-//     });
-
-//     const user = userData.get({ plain: true });
-
-//     res.render("profile", {
-//       ...user,
-//       logged_in: true
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-
-//cooments
-
-// router.get("/profile", withAuth, async (req, res) => {
-//   try {
-//     // Find the logged in user based on the session ID
-//     const userData = await User.findByPk(req.session.user_id, {
-//       attributes: { exclude: ["password"] },
-//       include: [{ model: Comments }],
-//     });
-
-//     const user = userData.get({ plain: true });
-
-//     res.render("profile", {
-//       ...user,
-//       logged_in: true
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-
-
+// GET route to show the login page
 router.get("/login", (req, res) => {
   
   // If the user is already logged in, redirect the request to another route
   if (req.session.loggedIn) {
+
+    // Redirect user to their personal profile
     res.redirect("/profile");
+
     return;
   }
 
+  // If the user is not logged in already,
+  // render the login handlebar
   res.render("login");
 });
 
